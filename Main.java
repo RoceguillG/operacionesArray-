9d8137c58cee0f1eb19f5c2228a7a847f8327970
Rocio Guisell López Espinoza <42458316+RoceguillG@users.noreply.github.com>
2022-08-10 19:39:43 +0000

    Version Final de como pedir las variables del metodo de gauss jordan y las respuestas de estas ecuaciones sacando las funcion del primer for

--- Main.java
+++ Main.java
@@ -11,30 +11,37 @@ class Main {
     double[][] array=new double [n][n+1];
     double resultado;
  
-
-    for (int fila = 0; fila < array.length; fila++) {
+    int fila=0,columna=n+1;
+    
+    for ( fila = 0; fila < array.length; fila++) {
      System.out.println("Ingrese los valores de fila "+(fila+1));
       
-      for (int columna = 0; columna < array[fila].length; columna++) {//ingresando
+      for ( columna = 0; columna < array[fila].length-1; columna++) {//ingresando
          System.out.println("ingrese el valor de X"+(columna+1));
-     
-            array[fila][columna]=sc.nextInt();
-
-
-        
-            if (array[fila][columna]== array.length) {
-              System.out.println("Ingrese valor de la respuesta de la Ecuacion: ");
+   
              array[fila][columna]=sc.nextInt();
-                    break   ;  
-     /* he puesto un break para que cierre el proceso del if en cuanto llegue a este punto, ya que si no lo hacia me volvi a pedir un numero para la ultima culumna y esto estaba mal ya que no era una incognita si no ya la rspuesta despues del = de la ecuacion.
+          // if (array[fila][columna]== array.length+1) {
+             //  System.out.println("Ingrese valor de la respuesta de la Ecuacion: ");
+             // array[fila][columna]=sc.nextInt(); break;
+       // }
+        } 
+      System.out.println("Ingrese valor de la respuesta de la Ecuacion: ");
+      array[fila][columna]=sc.nextInt();
+      
+      
+      
+      /* he puesto un break para que cierre el proceso del if en cuanto llegue a este punto, ya que si no lo hacia me volvi a pedir un numero para la ultima culumna y esto estaba mal ya que no era una incognita si no ya la rspuesta despues del = de la ecuacion.
               */
-            
-       }
+            //valio burgen el break
+       
 
-      }
+     
    
     
   }
+ System.out.println();
+       
+    
     for (int i = 0; i < array.length; i++) {
       for (int j = 0; j < array[i].length; j++) {
         System.out.print(array[i][j] + " ");
